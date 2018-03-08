298143f4d0e1e4f9994b50e708678df64933897f
AlexMarshaall <alexmarshaall@gmail.com>
2018-03-08 18:55:23 +0000

    not done much, michael asked me to pull to him

--- app/src/main/java/com/example/alex/MapChat/IndividualChat.java
+++ app/src/main/java/com/example/alex/MapChat/IndividualChat.java
@@ -18,27 +18,50 @@ import com.google.cloud.translate.Translation;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 public class IndividualChat extends AppCompatActivity {
-
-    /* //Trying to get the API key from metadata
-    try {
-        ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-        Bundle bundle = ai.metaData;
-        String myApiKey = bundle.getString("com.google.android.geo.API_KEY");
-    } catch (Exception e) {
-        Log.e(TAG, "Dear developer. Don't forget to configure <meta-data android:name=\"my_test_metagadata\" android:value=\"testValue\"/> in your AndroidManifest.xml file.");
-    }
-    */
 
     // todo API_KEY should not be stored in plain sight
     private static final String API_KEY = "AIzaSyA76E4nlLk0_AVd5XV-L4yIiLGWf7qkz3c";
 
     TextView textView;
-
     EditText inputText;
-
     Button translateButton;
+    static ArrayList<Message> messageArray;
+    static int messageId = 0;
+
+    public static class Message{
+        String username;
+        String messageContent;
+        int id;
+
+        public Message(String username, String messageContent) {
+            this.username = username;
+            this.id = messageId;
+            messageId++;
+            this.messageContent = messageContent;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getMessageId() {
+            return messageId;
+        }
+
+        public String getMessageContent() {
+            return messageContent;
+        }
+    }
+
+    static void populateMessageArrayList(){
+        messageArray.add(new Message("John","this is the first message"));
+        messageArray.add(new Message("Sally","this is the second message"));
+        messageArray.add(new Message("James","hi there everyone"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,24 +69,20 @@ public class IndividualChat extends AppCompatActivity {
         setContentView(R.layout.individual_chat);
 
          textView = findViewById(R.id.translatedText);
-
          inputText = findViewById(R.id.inputToTranslate);
-
          translateButton = findViewById(R.id.translateButton);
 
          translateButton.setOnClickListener(
                  new View.OnClickListener() {
                      public void onClick(View view){
                          String translated  = Translator.translate("hello","en","de");
-
                          textView.setText(translated);
-
                      }
 
                  }
          );
 
-        final Handler textViewHandler = new Handler();
+
 
     }
 
