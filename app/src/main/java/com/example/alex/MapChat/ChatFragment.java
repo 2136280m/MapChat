aa3c5dc6a0911817b3f0cb26bcbf3c26cf1921ee
AlexMarshaall <alexmarshaall@gmail.com>
2018-03-09 18:20:15 +0000

    Message fragment links to individual message

--- app/src/main/java/com/example/alex/MapChat/ChatFragment.java
+++ app/src/main/java/com/example/alex/MapChat/ChatFragment.java
@@ -3,35 +3,42 @@ package com.example.alex.MapChat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends ListFragment implements AdapterView.OnItemClickListener {
 
     // FOR NOW ALL THIS DOES IS TAKE YOU TO THE DEMO CHAT
 
     /* TODO each user will have an array of Chats, this fragment should iterate through them and
     * display them on the screen like facebook messenger does, showing a picture and the start
     * of the last message (can all be hardcoded in), would be good to implement a scroller function*/
-
-    private Button mDemoButton;
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
+        return view;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.ListOfChats, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
 
-        View view = inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
-        mDemoButton = (Button) view.findViewById(R.id.demo_button);
-        mDemoButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                    Intent intent = new Intent(getActivity(), IndividualChat.class);
-                    getActivity().startActivity(intent);
-            }
-        });
-        return view;
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Intent intent = new Intent(getContext(), IndividualChat.class);
+        startActivity(intent);
     }
 
 }
