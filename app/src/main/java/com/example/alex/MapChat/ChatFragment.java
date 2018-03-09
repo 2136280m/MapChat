08eb286b2caf2fb85cb35293b116b861f57f8283
AlexMarshaall <alexmarshaall@gmail.com>
2018-03-09 18:44:03 +0000

    asdf

--- app/src/main/java/com/example/alex/MapChat/ChatFragment.java
+++ app/src/main/java/com/example/alex/MapChat/ChatFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ChatFragment extends Fragment {
 
@@ -23,14 +26,34 @@ public class ChatFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
-        mDemoButton = (Button) view.findViewById(R.id.demo_button);
+
+/*        mDemoButton = (Button) view.findViewById(R.id.demo_button);
         mDemoButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                     Intent intent = new Intent(getActivity(), IndividualChat.class);
                     getActivity().startActivity(intent);
             }
+        });*/
+
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(imageAdapter.getItem(position).getImageUrlString()));
+                //startActivity(intent);
+
+                //TODO THIS IS NOT WORKING PROPERLY, FIND A WAY TO USE TO ACCESS THE CLASS FROM THE METHOD ABOVE
+
+                Intent intent = new Intent(getActivity(), IndividualMessage.class);
+                startActivity(intent);
+            }
         });
+
+
         return view;
     }
 
