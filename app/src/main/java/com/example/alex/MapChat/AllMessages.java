d18772c06a3ca7120109d35bab6b537b0f23a1b9
AlexMarshaall <alexmarshaall@gmail.com>
2018-03-09 15:38:37 +0000

    Almost got a chat thing

---
+++ app/src/main/java/com/example/alex/MapChat/AllMessages.java
@@ -0,0 +1,11 @@
+package com.example.alex.MapChat;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alex on 09/03/18.
+ */
+
+public class AllMessages {
+    public static ArrayList<IndividualMessage> allMessagesArray = new ArrayList<IndividualMessage>();
+}
